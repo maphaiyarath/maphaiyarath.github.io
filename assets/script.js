b2a583ce9e974c63b6c4aee9506efb266f536590
chelle <maphaiyarath@gmail.com>
2021-02-04 19:59:38 -0600

    adding js

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+/*
+var target = 'https://codingislove.com';
+
+$.ajax({
+    url: "https://api.linkpreview.net",
+    dataType: 'jsonp',
+    data: {q: target, key: 123456},
+    success: function (response) {
+        console.log(response);
+    }
+});
+*/
